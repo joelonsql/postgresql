a259bcd43e5fb9f41f1b1452ab12b33c453a6d82
Joel Jacobson <joel@compiler.org>
2024-10-16 18:33:38 +0200

    Change to /data

--- bench.sql
+++ bench.sql
@@ -22,13 +22,13 @@ SELECT now();
 \timing on
 
 -- COPY TO ... FORMAT text
-COPY t_1_a TO '/tmp/t_1_a.txt' (FORMAT text);
-COPY t_10_a TO '/tmp/t_10_a.txt' (FORMAT text);
-COPY t_100_a TO '/tmp/t_100_a.txt' (FORMAT text);
+COPY t_1_a TO '/data/t_1_a.txt' (FORMAT text);
+COPY t_10_a TO '/data/t_10_a.txt' (FORMAT text);
+COPY t_100_a TO '/data/t_100_a.txt' (FORMAT text);
 
-COPY t_1_mix TO '/tmp/t_1_mix.txt' (FORMAT text);
-COPY t_10_mix TO '/tmp/t_10_mix.txt' (FORMAT text);
-COPY t_100_mix TO '/tmp/t_100_mix.txt' (FORMAT text);
+COPY t_1_mix TO '/data/t_1_mix.txt' (FORMAT text);
+COPY t_10_mix TO '/data/t_10_mix.txt' (FORMAT text);
+COPY t_100_mix TO '/data/t_100_mix.txt' (FORMAT text);
 
 \timing off
 
@@ -41,13 +41,13 @@ VACUUM;
 \timing on
 
 -- COPY FROM ... FORMAT text
-COPY t_1_a FROM '/tmp/t_1_a.txt' (FORMAT text);
-COPY t_10_a FROM '/tmp/t_10_a.txt' (FORMAT text);
-COPY t_100_a FROM '/tmp/t_100_a.txt' (FORMAT text);
+COPY t_1_a FROM '/data/t_1_a.txt' (FORMAT text);
+COPY t_10_a FROM '/data/t_10_a.txt' (FORMAT text);
+COPY t_100_a FROM '/data/t_100_a.txt' (FORMAT text);
 
-COPY t_1_mix FROM '/tmp/t_1_mix.txt' (FORMAT text);
-COPY t_10_mix FROM '/tmp/t_10_mix.txt' (FORMAT text);
-COPY t_100_mix FROM '/tmp/t_100_mix.txt' (FORMAT text);
+COPY t_1_mix FROM '/data/t_1_mix.txt' (FORMAT text);
+COPY t_10_mix FROM '/data/t_10_mix.txt' (FORMAT text);
+COPY t_100_mix FROM '/data/t_100_mix.txt' (FORMAT text);
 
 \timing off
 
@@ -57,13 +57,13 @@ VACUUM;
 \timing on
 
 -- COPY TO ... FORMAT csv
-COPY t_1_a TO '/tmp/t_1_a.csv' (FORMAT csv);
-COPY t_10_a TO '/tmp/t_10_a.csv' (FORMAT csv);
-COPY t_100_a TO '/tmp/t_100_a.csv' (FORMAT csv);
+COPY t_1_a TO '/data/t_1_a.csv' (FORMAT csv);
+COPY t_10_a TO '/data/t_10_a.csv' (FORMAT csv);
+COPY t_100_a TO '/data/t_100_a.csv' (FORMAT csv);
 
-COPY t_1_mix TO '/tmp/t_1_mix.csv' (FORMAT csv);
-COPY t_10_mix TO '/tmp/t_10_mix.csv' (FORMAT csv);
-COPY t_100_mix TO '/tmp/t_100_mix.csv' (FORMAT csv);
+COPY t_1_mix TO '/data/t_1_mix.csv' (FORMAT csv);
+COPY t_10_mix TO '/data/t_10_mix.csv' (FORMAT csv);
+COPY t_100_mix TO '/data/t_100_mix.csv' (FORMAT csv);
 
 \timing off
 
@@ -76,13 +76,13 @@ VACUUM;
 \timing on
 
 -- COPY FROM ... FORMAT csv
-COPY t_1_a FROM '/tmp/t_1_a.csv' (FORMAT csv);
-COPY t_10_a FROM '/tmp/t_10_a.csv' (FORMAT csv);
-COPY t_100_a FROM '/tmp/t_100_a.csv' (FORMAT csv);
+COPY t_1_a FROM '/data/t_1_a.csv' (FORMAT csv);
+COPY t_10_a FROM '/data/t_10_a.csv' (FORMAT csv);
+COPY t_100_a FROM '/data/t_100_a.csv' (FORMAT csv);
 
-COPY t_1_mix FROM '/tmp/t_1_mix.csv' (FORMAT csv);
-COPY t_10_mix FROM '/tmp/t_10_mix.csv' (FORMAT csv);
-COPY t_100_mix FROM '/tmp/t_100_mix.csv' (FORMAT csv);
+COPY t_1_mix FROM '/data/t_1_mix.csv' (FORMAT csv);
+COPY t_10_mix FROM '/data/t_10_mix.csv' (FORMAT csv);
+COPY t_100_mix FROM '/data/t_100_mix.csv' (FORMAT csv);
 
 \timing off
 
